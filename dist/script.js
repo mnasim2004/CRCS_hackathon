d406dbe7a5783f97998cad74247846f114a79f05
Mohammed Nasim <mohammednasim2004@gmail.com>
2023-06-17 06:10:00 +0530

    sub chrts and click to redrect to sub charts left to do

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,47 @@
 
 
+var correspond = {
+  "IN-AN": "ANDAMAN AND NICOBAR ISLANDS",
+  "IN-AP": "ANDHRA PRADESH",
+  "IN-AR": "ARUNACHAL PRADESH",
+  "IN-AS": "ASSAM",
+  "IN-BR": "BIHAR",
+  "IN-CH": "CHANDIGARH",
+  "IN-CT": "CHHATTISGARH",
+  "IN-DL": "NEW DELHI",
+  "IN-DH": "DADRA AND NAGAR HAVELI AND DAMAN AND DIU",
+  "IN-GA": "GOA",
+  "IN-GJ": "GUJARAT",
+  "IN-HR": "HARYANA",
+  "IN-HP": "HIMACHAL PRADESH",
+  "IN-JK": "JAMMU AND KASHMIR",
+  "IN-JH": "JHARKHAND",
+  "IN-KA": "KARNATAKA",
+  "IN-KL": "KERALA",
+  "IN-LK": "LADAKH",
+  "IN-LD": "LAKSHADWEEP",
+  "IN-MP": "MADHYA PRADESH",
+  "IN-MH": "MAHARASHTRA",
+  "IN-MN": "MANIPUR",
+  "IN-ML": "MEGHALAYA",
+  "IN-MZ": "MIZORAM",
+  "IN-NL": "NAGALAND",
+  "IN-OR": "ODISHA",
+  "IN-PY": "PUDUCHERRY",
+  "IN-PB": "PUNJAB",
+  "IN-RJ": "RAJASTHAN",
+  "IN-SK": "SIKKIM",
+  "IN-TN": "TAMIL NADU",
+  "IN-TG": "TELANGANA",
+  "IN-TR": "TRIPURA",
+  "IN-UP": "UTTAR PRADESH",
+  "IN-UT": "UTTARAKHAND",
+  "IN-WB": "WEST BENGAL"
+}
 
+var states = Object.values(correspond)
+states = states.join("?").toLowerCase().split("?")
 /*----------End of India Chart---------------*/
-
 
 
 
@@ -72,14 +111,49 @@ function getSectorwise(datajson){
     return Sectorwise;
 }
 
+function getAreasofOperation(datajson){
+  var areas = {}
+  for (i in datajson){
+
+    list = datajson[i]["Area of Operation"].replace(".",",").split(",")
+    for (j in list){
+      if (areas[list[j].trim()]==null){
+        areas[list[j].trim()] = 1
+      }
+      else {
+        areas[list[j].trim()] +=1
+      }
+    }
+  }
+  //console.log("ssadasd")
+  //console.log(areas)
+  delete areas[""]
+  var newareas = {}
+  for (i in areas){
+    
+    var matchindex = stringSimilarity.findBestMatch(i, states).bestMatchIndex
+    newstate = states[matchindex]
+    //console.log(newstate)
+    if(newareas[newstate]==null){
+      newareas[newstate.toUpperCase()] = areas[i]
+    }
+    else{
+      newareas[newstate.toUpperCase()] += areas[i]
+    }
+  }
+
+  //console.log(newareas)
+  return newareas
+}
+
 
 function getYearWise(datajson){
-    for(i in datajson){
-        //console.log(datajson[i]["Date of Registration"]);
-        if(datajson[i]["Date of Registration"]==undefined){
-            console.log(datajson[i])
-        }
-    }
+    // for(i in datajson){
+    //     //console.log(datajson[i]["Date of Registration"]);
+    //     if(datajson[i]["Date of Registration"]==undefined){
+    //         console.log(datajson[i])
+    //     }
+    // }
     temp = {}
     for (var i = 0; i < datajson.length; i++) {
         if(datajson[i]["Date of Registration"]==null || datajson[i]["Date of Registration"]=="" ){
@@ -101,6 +175,53 @@ function getYearWise(datajson){
     return temp
 }
 
+
+
+
+function getSectorState(datajson,state){
+    var Sectorwise ={};
+    for (i in datajson){
+        if (datajson[i]["State"].toLowerCase()!=state.toLowerCase()){continue}
+
+        if(Sectorwise[datajson[i]["Sector Type"]] == null){
+            Sectorwise[datajson[i]["Sector Type"]] = 1
+        } 
+        else{
+            Sectorwise[datajson[i]["Sector Type"]] += 1
+        }
+    }
+    console.log(Sectorwise);
+    return Sectorwise;
+}
+
+
+
+
+function getYearState(datajson,state){
+    temp = {}
+    for (var i = 0; i < datajson.length; i++) {
+        if((datajson[i]["Date of Registration"]==null || datajson[i]["Date of Registration"]=="" )&&(datajson[i]["State"] == state )){
+            if (!temp[""]){
+                temp[""] = 1
+            }
+            else{
+                temp[""] += 1 
+            }
+        }
+
+        else if(( temp[datajson[i]["Date of Registration"].split("/")[2]]==null)&&(datajson[i]["State"] == state )){
+            temp[datajson[i]["Date of Registration"].split("/")[2]] = 1
+        }
+        else if (datajson[i]["State"] == state ){
+            temp[datajson[i]["Date of Registration"].split("/")[2]] += 1;
+        }
+    }
+    return temp
+}
+
+
+
+
 $(document).ready(function() {
   //console.log("asdasdad")
   $.ajax({
@@ -109,12 +230,18 @@ $(document).ready(function() {
       dataType: "text",
       success: function(data) {
           //console.log(data)
+          var state = "Kerala"  
           var datajson = $.csv.toObjects(data)
           createTable(datajson)
-          createMap(datajson)
-          createChart(datajson)
+          createMapStatewise(datajson)
+          createMapAreas(datajson)
+          //console.log(getAreasofOperation(datajson))
+          //getSectorState(datajson,state)
+          //getYearState(datajson,state)
+          createCharts(datajson)
           console.log(datajson)
-          getYearWise(datajson)
+          //getYearWise(datajson)
+          
       },
       error: function (request, status, error) {
           //console.log(error)
